eef223f125de1402aae482cf386c8e317070ce39
tudor.miron <tudor.miron@mta.ro>
2024-05-20 08:50:25 +0300

    changed token validation url

--- fittnes_frontend/lib/pages/login_page.dart
+++ fittnes_frontend/lib/pages/login_page.dart
@@ -43,7 +43,7 @@ class _LoginPageState extends State<LoginPage> {
       String loginDataJson = json.encode(loginData);
 
       Uri url =
-          Uri.parse('http://localhost:8080/api/v1/auth/login'); // Use Uri.http
+          Uri.parse('http://172.205.42.172/api/v1/auth/login'); // Use Uri.http
 
       Response response =
           await post(url, headers: headers, body: loginDataJson);
@@ -98,7 +98,7 @@ class _LoginPageState extends State<LoginPage> {
           String loginDataJson = json.encode(loginData);
 
           Uri url =
-              Uri.parse('http://192.168.54.182:8080/api/v1/auth/loginGoogle');
+              Uri.parse('http://172.205.42.172/api/v1/auth/loginGoogle');
 
           Response response =
               await post(url, headers: headers, body: loginDataJson);
