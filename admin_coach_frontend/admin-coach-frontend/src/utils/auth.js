e206184dae4f76063f04a7d38ba7424a428c831c
tudor.miron <tudor.miron@mta.ro>
2024-05-23 20:00:51 +0300

    changed version of frontend app

--- admin_coach_frontend/admin-coach-frontend/src/utils/auth.js
+++ admin_coach_frontend/admin-coach-frontend/src/utils/auth.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const validateToken = async (token) => {
   try {
-    const response = await axios.get(`http://localhost:8080/api/v1/auth/validateToken?token=${encodeURIComponent(token)}`);
+    const response = await axios.get(`https://www.fit-stack.online/api/v1/auth/validateToken?token=${encodeURIComponent(token)}`);
     return response.data;
   } catch (error) {
     console.error("Error validating token:", error);
