93899c2f8b14b940524dff9506fe5d645c715091
tudor.miron <tudor.miron@mta.ro>
2024-05-27 20:00:13 +0300

    added the correct open api endpoint for webhook

--- fittness-app/parent-module/api-gateway/src/main/java/tudor/work/filter/RouteValidator.java
+++ fittness-app/parent-module/api-gateway/src/main/java/tudor/work/filter/RouteValidator.java
@@ -16,7 +16,7 @@ public class RouteValidator {
             "/api/v1/auth/googleAuth",
             "/api/v1/auth/validateToken/**",
             "/api/selfCoach/paypal/getPayPalSubscriptionButton",
-            "/api/selfCoach/webhookResponse",
+            "/api/selfCoach/user/webhookResponse",
             "/login/oauth2/code/google",
             "/eureka"
     );
